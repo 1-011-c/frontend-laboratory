6ab34d023921fcf68a4b1d389ff62a9f0770eba5
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-03-21 15:14:46 +0100

    DIO Timeout + URL validation

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,14 +1,17 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
-import 'package:convert/convert.dart';
 import 'package:labor_scanner/model/corona_test_case.dart';
 
 class APIService {
   static const API_BASE_PATH = "https://google.com/";
-  static Dio dio = new Dio();
+  static const API_TIMEOUT = 3000;
+  static Dio dio = new Dio(BaseOptions(connectTimeout: API_TIMEOUT));
 
   static Future<CoronaTestCase> update(final String url, final CoronaStatus infected) async {
+    if (!url.startsWith(API_BASE_PATH))
+      return null;
+
     final Response response = await dio.patch(url);
 
     if (response.statusCode != 200)
