9f4a7a5950f89087f3afcc5dd5247b247fcdc465
Justin <justin.vietz@gmx.de>
2020-03-21 21:58:44 +0100

    Corrected for new barcodes.

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -4,7 +4,11 @@ import 'package:dio/dio.dart';
 import 'package:labor_scanner/model/corona_test_case.dart';
 
 class APIService {
-  static const API_BASE_PATH = "https://blffmaku9b.execute-api.eu-central-1.amazonaws.com/Prod/corona-test-case";
+  static const API_BASE_URL = 'https://blffmaku9b.execute-api.eu-central-1.amazonaws.com/Prod';
+
+  /// Used to test if data in barcode is valid
+  static const API_BASE_PATH = "/corona-test-case";
+  static const API_URL = '$API_BASE_URL$API_BASE_PATH';
   static const API_TIMEOUT = 3000;
   static Dio dio = new Dio(BaseOptions(connectTimeout: API_TIMEOUT));
 
@@ -12,7 +16,9 @@ class APIService {
     if (!url.startsWith(API_BASE_PATH))
       return null;
 
-    final Response response = await dio.patch('$url/${infected.toString().split(".")[1]}');
+    final finalUrl = '$API_BASE_URL$url/${infected.toString().split(".")[1]}';
+    print(finalUrl);
+    final Response response = await dio.patch(finalUrl);
 
     if (response.statusCode != 200)
       return null;
